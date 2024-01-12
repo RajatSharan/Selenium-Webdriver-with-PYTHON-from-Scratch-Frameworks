2634cdcf8db6e85c7066ec8fc3fbc54a187bbeb8
Rajat Sharan <errajatsharan@gmail.com>
2024-01-12 19:03:37 +0530

    Handling Static dropdowns

--- pythonSelenium/locators.py
+++ pythonSelenium/locators.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 
 #ChromeDriver
 
@@ -11,6 +12,14 @@ driver.find_element(By.CSS_SELECTOR,"input[name='name']").send_keys("Rajat")
 driver.find_element(By.NAME,"email").send_keys("errajat.shara@gmail.com")
 driver.find_element(By.ID,"exampleInputPassword1").send_keys("12345")
 driver.find_element(By.ID,'exampleCheck1').click()
+driver.find_element(By.NAME,"inlineRadioOptions").click()
+
+# Static DropDown
+
+dropDown = Select(driver.find_element(By.ID,"exampleFormControlSelect1"))
+dropDown.select_by_index(1)
+dropDown.select_by_visible_text("Female")
+dropDown.select_by_index(0)
 
 #//tagname[@attribute='value']
 
